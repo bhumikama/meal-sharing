f7af051d936db23f07811cd8e2f49cfbf5077aea
Bhumika Mallikarjun <bhumikamallikarjun98@gmail.com>
2024-09-16 21:59:25 +0200

    updated first_meal and last_meal to handle when no meals are found

--- api/src/routers/first-meal.js
+++ api/src/routers/first-meal.js
@@ -7,7 +7,13 @@ const firstMealRouter = express.Router();
 firstMealRouter.get('/', async (req, res) => {
     try {
         const first_meal =  await knex('Meal').orderBy('id','asc').first();
-        res.json(first_meal);
+        if(first_meal){
+            res.json(first_meal);
+        }
+        else{
+            res.status(404).json({message : "No meals found"});
+        }
+        
     } catch (err) {
       res.status(500).json({ error: err.message });
     }
--- api/src/routers/last-meal.js
+++ api/src/routers/last-meal.js
@@ -7,7 +7,13 @@ const lastMealRouter = express.Router();
 lastMealRouter.get('/', async (req, res) => {
     try {
         const last_meal =  await knex('Meal').orderBy('id','desc').first();
-        res.json(last_meal);
+        if(last_meal){
+            res.json(last_meal);
+        }
+        else{
+            res.status(404).json({message : "No meals found"});
+        }
+        
     } catch (err) {
       res.status(500).json({ error: err.message });
     }
