3f2e426c758fb252c2e481467c7c54099a1f625a
Bhumika Mallikarjun <bhumikamallikarjun98@gmail.com>
2024-10-31 22:06:03 +0100

    displayed the meal card in a grid layout

---
+++ meal-sharing/src/app/meals/page.js
@@ -0,0 +1,15 @@
+import MealList from "@/components/Meals/MealList";
+async function fetchMeals() {
+  try {
+    const apiResponse = await fetch("http://localhost:3001/api/meals");
+    const data = await apiResponse.json();
+    return data;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+export default async function Recipes() {
+  const mealList = await fetchMeals();
+  return <MealList mealsList={mealList} />;
+}
